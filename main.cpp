f0ddaf95e7bc7ac7c6fab6c145459e59e632bfa8
Nicholas Miller <nicholascmiller123@gmail.com>
2021-09-16 22:21:03 -0400

    Finished project!

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ char generateGuess(string word);
 
 // Global Variables
 forward_list<string>* dictionary;
+string guesses;
 
 int main() {
 	int numLetters;
@@ -23,29 +24,50 @@ int main() {
 		cout << "Failed to read dictionary file" << endl;
 		return 1;
 	}
+	guesses = "";
 
 	string word;
 	for (int i = 0; i < numLetters; i++) {
-		word += '.';
+		word += '_';
 	}
 	cout << word << endl;
 
 	string input = "q";
-	while (word.find('.') != string::npos) {
+	while (word.find('_') != string::npos) {
 		cout << "Suggested Guess: " << generateGuess(word) << endl;
-		cout << "Enter updated word (use . for unknown) - 'q' to exit: " << endl;
+		cout << "Did the guess succeed? (y/n) - 'q' to exit: " << endl;
 		cin >> input;
 
 		if (input == "q") {
 			return 0;
 		}
-
-		while (input.length() != numLetters) {
-			cout << "Incorrect number of letters, please try again" << endl;
+		else if (input == "y") {
+			cout << "Enter updated word (use _ for unknown) - 'q' to exit: " << endl;
 			cin >> input;
+
+			while (input.length() != numLetters) {
+				cout << "Incorrect number of letters, please try again" << endl;
+				cin >> input;
+			}
+
+			word = input;
+			for (unsigned int i = 0; i < word.length(); i++) {
+				char letter = word.at(i);
+				if (letter != '_' && guesses.find(letter) == string::npos) {
+					guesses.append(string(1, letter));
+				}
+			}
+		}
+		else if (input == "n") {
+			cout << "Enter the guessed letter: " << endl;
+			cin >> input;
+
+			guesses.append(input);
+		}
+		else {
+			cout << "Not a valid response" << endl;
 		}
 
-		word = input;
 		cout << "----------------------------" << endl;
 	}
 
@@ -77,13 +99,19 @@ bool readInDictionary(string filename, int wordLength) {
 
 char generateGuess(string word) {
 	// Removes all words from the dictionary that don't match the current word
-	// TODO: Remove words with too many of guessed letters
-	regex pattern = regex(word, regex_constants::optimize);
+	string regexPattern = word;
+	string plugIn = guesses.length() == 0 ? "." : "[^" + guesses + "]";
+	for (unsigned int i = 0; i < regexPattern.length(); i++) {
+		if (regexPattern.at(i) == '_') {
+			regexPattern.replace(i, 1, plugIn);
+		}
+	}
+	regex pattern = regex(regexPattern, regex_constants::optimize);
 	dictionary->remove_if([&pattern](string word) -> bool { return !regex_match(word, pattern); });
 
 	// Count character occurences in words
-	// TODO: prevent suggesting the same character twice
 	unordered_map<char, int> characterCount;
+	characterCount.emplace('_', 0);
 	for (auto itr = dictionary->begin(); itr != dictionary->end(); itr++) {
 		for (char c : *itr) {
 			if (characterCount.count(c) == 0) {
@@ -97,8 +125,15 @@ char generateGuess(string word) {
 
 	// Choose the letter with the most occurences
 	auto suggestion = characterCount.begin();
+	while (guesses.find(suggestion->first) != string::npos) {
+		suggestion++;
+	}
+	cout << suggestion->first << endl;
 	for (auto itr = characterCount.begin(); itr != characterCount.end(); itr++) {
-		if (itr->second > suggestion->second) {
+		// TODO: prevent suggesting the same character twice
+		if (guesses.find(itr->first) == string::npos && itr->second > suggestion->second) {
+			cout << guesses << " | " << itr->first << " | " << itr->second << " | " << suggestion->second << endl;
+			cout << (guesses.find(itr->first) == string::npos) << endl;
 			suggestion = itr;
 		}
 	}
